f0a3b7f38bb84191c702191d498bd3859f5b47db
Arina <Arina@DESKTOP-H3T0JAQ>
2023-10-15 15:37:05 +0200

    Добавьте файлы проекта.

---
+++ solodkaya.lab2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y:");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine (y <= -x + 2 && y >= -x + 1 && (y <= x - 1 && y >= x - 2));
